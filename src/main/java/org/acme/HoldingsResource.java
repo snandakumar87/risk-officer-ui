e7db5ef826f8bcd8869917f6f22b062bbd9045c5
snandakumar87 <snandaku@redhat.com>
2021-10-05 13:56:27 -0400

    changed

--- src/main/java/org/acme/HoldingsResource.java
+++ src/main/java/org/acme/HoldingsResource.java
@@ -115,7 +115,7 @@ public class HoldingsResource {
         VarCalculationRequest respo = getVarCalculationResponse(body, confidence, entity, id,false);
         respo.setCorrelationId(uuid);
         kafkaController.produce(uuid,new ObjectMapper().writeValueAsString(respo));
-        Thread.sleep(1000);
+        Thread.sleep(2000);
         String resp = getCase(uuid);
         Map respMap = new ObjectMapper().readValue(resp,Map.class);
         System.out.println(respMap.keySet());
