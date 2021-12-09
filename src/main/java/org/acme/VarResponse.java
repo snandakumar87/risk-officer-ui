cd54ba86466a31f9ed6ffaf69c11a189e4569043
snandakumar87 <snandaku@redhat.com>
2021-12-09 12:59:04 -0500

    Added missing java class

---
+++ src/main/java/org/acme/VarResponse.java
@@ -0,0 +1,22 @@
+package org.acme;
+
+public class VarResponse {
+    String entityId;
+    String varResults;
+
+    public String getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(String entityId) {
+        this.entityId = entityId;
+    }
+
+    public String getVarResults() {
+        return varResults;
+    }
+
+    public void setVarResults(String varResults) {
+        this.varResults = varResults;
+    }
+}
